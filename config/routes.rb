3fc367ca772c6b52455e013b404cc7577449206f
seairs <sea.airs77@gmail.com>
2022-05-14 22:41:07 -0400

    updating new, show html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :tweets
+  resources :tweets 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweets#index"
 end
